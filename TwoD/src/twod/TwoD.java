6627b524327630b1e708eb41c6ba477c678883c1
Nelson Álvarez <nelsonalva@gmail.com>
2022-04-16 18:14:11 -0500

    - Creados los 4 proyectos - completado proyecto Brujita - completado proyecto Panda

--- TwoD/src/twod/TwoD.java
+++ TwoD/src/twod/TwoD.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pkg2d;
+package twod;
 
 /**
  *
  * @author nelso
  */
-public class Main {
+public class TwoD {
 
     /**
      * @param args the command line arguments
